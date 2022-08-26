7b057ec681009fda1f89eb3fa6cd09e0420ab05a
Amir <amir.akbrzadh3@gmail.com>
2022-08-26 14:58:55 +0430

    debugging some bugs

--- src/FormWrapper.js
+++ src/FormWrapper.js
@@ -1,6 +1,14 @@
+import { getAllByText } from "@testing-library/react";
+import { getValue } from "@testing-library/user-event/dist/utils";
+import { useState } from "react";
+
 const FormWrapper =() => {
+     const [arr , setArr] = useState(0) ; 
+     function updatearr() {
+          setArr(arr + 1) ; 
+     }
      return (
-          <div className="flex flex-col gap-[1rem] mt-[25rem] sm:mt-[8rem] px-[5%] lg:pr-[25%] lg:mt-[14rem] lg:ml-[10rem]">
+          <div className="flex flex-col gap-[1rem] mt-[8rem] lg:ml-[15rem] px-[10%] lg:pl-[0%] lg:pr-[20%] lg:justify-center lg:ml-[14rem] mb-[2rem] max-w-[1000px]">
                <label className="flex flex-col gap-2 text-[1rem] font-bold items-center xsm:items-start">
                     CARDHOLDER NAME
                     <input type='text' className="w-full border-[.15rem] rounded-[.5rem]  py-2 px-4 focus:border-[#6f00ff] focus:outline-none focus:ring-0 outline-none" maxLength='15' placeholder="e.g. Jabe Applesed"/>
@@ -12,10 +20,10 @@ const FormWrapper =() => {
                <div className="grid xsm:grid-cols-1fr1fr gap-[1rem]">
                     <div className="flex flex-col gap-2 text-[1rem] font-bold items-center xsm:items-start">
                          <label>
-                              EXP. DATE (MM/YY)
+                              Date
                          </label>
                          <div className="grid grid-cols-1fr1fr gap-[.5rem]">
-                              <input type='text' className="w-full border-[.15rem] rounded-[.5rem]  py-2 px-4 focus:border-[#6f00ff] focus:outline-none focus:ring-0 outline-none" maxLength='2' placeholder="MM" />
+                              <input onChange={() => {setArr(document.querySelector('.input').value)}} type='text' className="w-full border-[.15rem] rounded-[.5rem]  py-2 px-4 focus:border-[#6f00ff] focus:outline-none focus:ring-0 outline-none input" maxLength='19' placeholder="MM" value={arr} />
                               <input type='text' className="w-full border-[.15rem] rounded-[.5rem]  py-2 px-4 focus:border-[#6f00ff] focus:outline-none focus:ring-0 outline-none" maxLength='2' placeholder="YY" />
                          </div>
                     </div>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import LeftSide from './leftSide';
 import FormWrapper from './FormWrapper';
 const Main = () => {
      return (
-          <main className='flex flex-col relative md:grid md:grid-cols-1fr2fr min-h-[100vh]'>
+          <main className='flex flex-col relative md:grid md:grid-cols-1fr2fr min-h-[100vh] gap-[2rem] '>
                <LeftSide />
                <FormWrapper />
           </main>
